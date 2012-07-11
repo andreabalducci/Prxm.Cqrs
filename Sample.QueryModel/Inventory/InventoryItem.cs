942037dbf5145c02bd4d26d3cdc2c20e34ad611d
Andrea Balducci <mtb.snowboard@gmail.com>
2012-07-11 23:16:26 +0200

    added storage availability to inventory item query model

--- Sample.QueryModel/Inventory/InventoryItem.cs
+++ Sample.QueryModel/Inventory/InventoryItem.cs
@@ -7,15 +7,30 @@ namespace Sample.QueryModel.Inventory
 {
     public class InventoryItem
     {
+        public class Availability
+        {
+            public string Storage { get; protected set; }
+            public decimal Quantity { get; protected set; }
+
+            public Availability(string storage, decimal quantity)
+            {
+                Storage = storage;
+                Quantity = quantity;
+            }
+        }
+
         public Guid Id { get; protected set; }
         public string Sku { get; protected set; }
         public string Description { get; protected set; }
+        public IList<Availability> StorageAvailability { get; protected set; }
+
 
         public InventoryItem(Guid id, string sku, string description)
         {
             Id = id;
             Sku = sku;
             Description = description;
+            this.StorageAvailability = new List<Availability>();
         }
     }
 }

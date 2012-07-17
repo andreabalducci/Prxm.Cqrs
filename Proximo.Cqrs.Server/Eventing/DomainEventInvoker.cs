0be6996ed2aaee2473240241e871a9041d4dd282
Alessandro Giorgetti <guardian@nablasoft.com>
2012-07-17 11:48:55 +0200

    removed unneeded files, added missing

---
+++ Proximo.Cqrs.Server/Eventing/DomainEventInvoker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proximo.Cqrs.Server.Eventing
+{
+	/// <summary>
+	/// a class that holds some detailed information about the functions that are used to handle 
+	/// </summary>
+	public class DomainEventInvoker
+	{
+		// the actual function that have to be invoked in order to handle the event
+		public Action<IDomainEvent> Invoker;
+
+		/// <summary>
+		/// the type in which the onvoker is defined
+		/// </summary>
+		public Type DefiningType;
+
+		public DomainEventInvoker(Action<IDomainEvent> invoker, Type definingType)
+		{
+			Invoker = invoker;
+			DefiningType = definingType;
+		}
+
+		public void Invoke(IDomainEvent @event)
+		{
+			Invoker.Invoke(@event);
+		}
+	}
+}
--- Proximo.Cqrs.Server/Eventing/IDomainEventHandlerCatalog.cs
+++ Proximo.Cqrs.Server/Eventing/IDomainEventHandlerCatalog.cs
@@ -16,29 +16,4 @@ namespace Proximo.Cqrs.Server.Eventing
         IEnumerable<DomainEventInvoker> GetAllHandlerFor(Type domainEventType);
         //IEnumerable<Action<IDomainEvent>> GetAllHandlerFor(Type domainEventType);
     }
-
-    /// <summary>
-    /// a class that holds some detailed information about the functions that are used to handle 
-    /// </summary>
-    public class DomainEventInvoker
-    {
-        // the actual function that have to be invoked in order to handle the event
-        public Action<IDomainEvent> Invoker;
-
-        /// <summary>
-        /// the type in which the onvoker is defined
-        /// </summary>
-        public Type DefiningType;
-
-        public DomainEventInvoker(Action<IDomainEvent> invoker, Type definingType)
-        {
-            Invoker = invoker;
-            DefiningType = definingType;
-        }
-
-        public void Invoke(IDomainEvent @event)
-        {
-            Invoker.Invoke(@event);
-        }
-    }
 }

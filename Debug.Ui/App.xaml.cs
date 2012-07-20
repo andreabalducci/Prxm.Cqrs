4a89c95d92065a4e9f58cbfea7ee7a98d9a1c9ff
Gian Maria <ricci.gianmaria@gmail.com>
2012-07-20 18:16:29 +0200

    added minimal infrastructure to incercept log4netlogger to create a wpf console to debug infrastructure

--- Debug.Ui/App.xaml.cs
+++ Debug.Ui/App.xaml.cs
@@ -4,13 +4,49 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
-
-namespace Debug.Ui
+using Sample.DebugUi.Infrastructure;
+using System.Windows.Threading;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Sample.DebugUi.KissMvvm;
+using System.Reflection;
+using Sample.DebugUi.Views;
+namespace Sample.DebugUi
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private static Dispatcher _uiDispatcher;
+        internal WindsorContainer Container;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _uiDispatcher = this.Dispatcher;
+            Container = new WindsorContainer();
+            Container.Register(
+                    Classes.FromAssembly(Assembly.GetExecutingAssembly())
+                    .BasedOn<BaseViewModel>()
+                    .LifestyleTransient());
+            Container.Register(
+               Classes.FromAssembly(Assembly.GetExecutingAssembly())
+               .InNamespace("Sample.DebugUi.Views")
+               .LifestyleTransient());
+
+            var viewStart = Container.Resolve<RawLoggerView>();
+            viewStart.Show();
+            UdpInterceptor interceptor = new UdpInterceptor();
+            interceptor.Start();
+        }
+
+        public static void ExecuteInUiThread(Action action)
+        {
+            _uiDispatcher.Invoke((Delegate)action, new Object[] { });
+
+        }
+
+     
     }
 }
---
+++ Debug.Ui/Infrastructure/UdpInterceptor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Sample.DebugUi.Infrastructure
+{
+    public class UdpInterceptor
+    {
+        private UdpClient _udpClient;
+        private Boolean _stop;
+        private IPEndPoint _ipe;
+
+        public UdpInterceptor()
+        {
+            try
+            {
+                _udpClient = new UdpClient(8080);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error when opening socked: " + ex.ToString());
+            }
+
+            Sleeping = false;
+
+        }
+
+        public bool Sleeping { get; set; }
+
+        public void Close()
+        {
+            _udpClient.Close();
+        }
+
+        public void Start()
+        {
+            ThreadPool.QueueUserWorkItem(Start, null);
+        }
+        private void Start(Object state)
+        {
+            while (true)
+            {
+                if (_stop)
+                    break;
+                try
+                {
+                    byte[] sent = _udpClient.Receive(ref _ipe);
+
+                }
+                catch (SocketException e)
+                {
+
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+    }
+}
